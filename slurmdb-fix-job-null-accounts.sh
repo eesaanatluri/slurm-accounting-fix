db910181caf6e1bd4a662133c097fbe29e2a3e21
Cloud User <centos@ohpc.novalocal>
2019-11-08 22:02:42 +0000

    Applied following changes for better Readability of code
    1) Changed variable data1 to slurm_assocs.
    2) sql query inside the `` rather than including them inside $() and changed the <<< to use -e in the command.
    3) removed user variable as it was not used in the code.
    4) Added progress percent tracker.
    5) Added print staments for sanity checking and debugging if required.

--- slurmdb-fix-job-null-accounts.sh
+++ slurmdb-fix-job-null-accounts.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+total_jobs=`mysql -N -u root -D slurmdb -e "select count(1) from ohpc_job_table where account IS NULL or id_assoc=0;"`
+echo "Total number of jobs to be updated = $total_jobs"
+if [ $total_jobs = "0" ]; then
+	exit 1
+fi
+total_jobs_updated=0
 while read -r line
 do
+	progress=$(( total_jobs_updated*100/total_jobs ))
+	
+	echo -en "\rPROGRESS: $progress %"
+	if [ $progress = "100" ]; then
+        	break;
+	fi
 	uid=`echo $line | cut -d ',' -f1`
 	slurm_user_name=`echo $line | cut -d ',' -f2`
-	data1=$(mysql -u root -D slurmdb<<<"SELECT distinct(id_assoc) , acct, user  FROM ohpc_assoc_table  where user='$slurm_user_name';")
-	id_assoc=`echo $data1 | cut -d ' ' -f4`
-	acct=`echo $data1 | cut -d ' ' -f5`
-	user=`echo $data1 | cut -d ' ' -f6`
-	data1=$(mysql -u root -D slurmdb<<<"UPDATE ohpc_job_table  set id_assoc='$id_assoc' where id_user='$uid' and id_assoc=0;\
-UPDATE ohpc_job_table  set account='$acct' where id_user='$uid' and account='';") 
+	slurm_assocs=`mysql -N -u root -D slurmdb -e "SELECT distinct(id_assoc), acct  FROM ohpc_assoc_table  where user='$slurm_user_name';"`
+	id_assoc=`echo $slurm_assocs | cut -d ' ' -f1`
+	acct=`echo $slurm_assocs | cut -d ' ' -f2`
+	#echo "updating user with id_assoc with account name and UID = "$id_assoc, $acct "and "$uid
+	
+	row_count_id_0=`mysql -N -u root -D slurmdb -e "UPDATE ohpc_job_table  set id_assoc='$id_assoc' where id_user='$uid' and id_assoc=0;\
+		SELECT ROW_COUNT();"`
+	row_count_accnt=`mysql -N -u root -D slurmdb -e "UPDATE ohpc_job_table  set account='$acct' where id_user='$uid' and account='' or account IS NULL;\
+		SELECT ROW_COUNT();"`
+	total_jobs_updated=$(( total_jobs_updated+row_count_accnt+row_count_id_0 ))	 
+	
 done<"$1"
+printf "\n Total jobs updated = $total_jobs_updated\n"
+
+
+
+
