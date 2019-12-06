b6d0cdda1977a11c49eda0d11d42c8b9ada049aa
Chirag Chandrahas Shetty <chirag24@uab.edu>
2019-12-06 14:28:34 -0600

    Update the missing id_assoc and account columns in the slurm job table.

--- slurmdb-fix-job-null-accounts.sh
+++ slurmdb-fix-job-null-accounts.sh
@@ -4,15 +4,17 @@ while read -r line
 do
 
 	uid=`echo $line | cut -d ',' -f1`
-	slurm_acct_name=`echo $line | cut -d ',' -f2`
+	slurm_user_name=`echo $line | cut -d ',' -f2`
+	##Getting the missing assoc id from ohpc_assoc_table
+	slurm_assocs=`mysql -N -u root -D slurmdb -e "SELECT distinct(id_assoc), acct  FROM ohpc_assoc_table  where user='$slurm_user_name';"`
+	id_assoc=`echo $slurm_assocs | cut -d ' ' -f1`
+	acct=`echo $slurm_assocs | cut -d ' ' -f2`
+	#echo "updating user with id_assoc with account name and UID = "$id_assoc, $acct "and "$uid
 
-	total_jobs=`mysql -N -u root -D slurmdb -e "select count(1) from ohpc_job_table where id_user=$uid and account IS NULL;"`
+	##Updating the missing data in the table in two statements because in some of the cases id_assoc is missing but account name is present.
+	row_count_id_0=`mysql -N -u root -D slurmdb -e "UPDATE ohpc_job_table  set id_assoc='$id_assoc' where id_user='$uid' and id_assoc=0;\
+		SELECT ROW_COUNT();"`
+	row_count_accnt=`mysql -N -u root -D slurmdb -e "UPDATE ohpc_job_table  set account='$acct' where id_user='$uid' and account='' or account IS NULL;\
+		SELECT ROW_COUNT();"`
 
-	#printf "User $slurm_acct_name with user ID $uid has $total_jobs jobs with account name null \n"
-
-	#printf "Updating $total_jobs records for user $slurm_acct_name with user ID $uid \n"
-
-	printf "UPDATE ohpc_job_table SET account=\'$slurm_acct_name\' WHERE id_user=$uid and account IS NULL;\n"
-
-	#mysql -u root -D slurmdb -e \"UPDATE ohpc_job_table SET account=$slurm_acct_name WHERE id_user=$uid and account IS NULL;\"
 done<"$1"
